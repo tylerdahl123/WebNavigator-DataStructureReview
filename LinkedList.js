c08ecc5bdab6f75131c9aabbb7aefebc2f7cc158
tyler dahlgren <dahlgren15@gmail.com>
2021-03-17 15:03:34 -0700

    added the linked list and the stack data structure from a pervious exercise

---
+++ LinkedList.js
@@ -0,0 +1,69 @@
+const Node = require('./node');
+
+class LinkedList {
+    constructor(){
+        this.head = null;
+    }
+
+    addToHead(data){
+        const newHead = new Node(data);
+        const currentHead = this.head;
+        this.head = newHead;
+        if(currentHead){
+            this.head.setNextNode(currentHead);
+        }
+    }
+addToTail(data){
+    let tail = this.head;
+    if(!tail){
+        this.head = new Node(data);
+    } else {
+        while (tail.getNextNode() !== null) {
+            tail = tail.getNextNode();
+        }
+        tail.setNextNode(new Node(data));
+    }
+}
+
+removeHead(){
+    const removedHead = this.head;
+    if(!removedHead){
+        this.head = removedHead.next;
+    }
+    return removedHead.data;
+}
+
+printList(){
+    let currentNode = this.head;
+    let output = '<head> ';
+    while( currentNode !==null){
+        output += currentNode.data + ' ';
+        currentNode = currentNode.next;
+    }
+    output += `<tail>`;
+    console.log(output);
+}
+findNodeIteratively(data){
+    let currentNode = this.head;
+    while(currentNode !== null){
+        if (currentNode.data === data){
+            return currentNode;
+        }
+        currentNode = currentNode.next;
+    }
+    return null;
+}
+
+findNodeRecusively(data, currentNode = this.head){
+    if (current === null){
+        return null;
+    } else if (currentNode.data ===data){
+        return currentNode;
+    } else {
+        return this.findNodeRecusively(data, currentNode.next);
+    }
+}
+
+}
+
+module.exports = LinkedList;
