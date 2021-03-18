5f1dbdd0abd59da22299799ea84b4eb818471f62
tyler dahlgren <dahlgren15@gmail.com>
2021-03-18 14:01:18 -0700

    managed to add the final logic

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ const prompt = require('prompt-sync')();
 // ------------------------------
 const backPages = new Stack();
 
-const NextPages = new Stack();
+const nextPages = new Stack();
 
 let currentPage = 'Home Page';
 // ------------------------------
@@ -57,8 +57,39 @@ let showBack = false;
 let showNext = false; 
 showCurrentPage("Current: ");
 while(finish === false){
-  var instructions = baseInfo;
-}
+  let instructions = baseInfo;
+  if(backPages.peek() != null){
+    instructions= `${instructions}, ${backInfo}`;
+    showBack = true;
+  }else {
+      showBack = false;
+  }
+  if(nextPages.peek() !=null){
+    instructions = `${instructions}, ${nextInfo}`;
+    showNext = true;
+  }
+  else {
+    showNext = false;
+  }
+  instructions = `${instructions}, ${quitInfo}.`
+  console.log(instructions);
   // ------------------------------
   // User Interface Part 2
   // ------------------------------
+
+const answer = prompt(question);
+const lowerCase = answer.toLowerCase();
+if((lowerCase !=='n') && (lowerCase !='b') && (lowerCase !=='q')){
+  newPage(answer);
+} else if ((showNext == true) && (lowerCase =='n')){
+  nextPage();
+} else if ((showBack == true) && (lowerCase =='b')){
+  backPage();
+} else if (lowerCase ==='b') {
+  console.log("Cannot go back a page. Stack is Empty.");
+} else if (lowerCase ==='n'){
+  console.log("Cannot go to next page. Stack is Empty.");
+} else if (lowerCase==='q'){
+finish= true;
+}
+}
