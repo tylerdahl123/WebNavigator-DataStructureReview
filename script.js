e96d7534a19895468c03915fcd36c09539278e11
tyler dahlgren <dahlgren15@gmail.com>
2021-03-18 12:41:22 -0700

    added some helper functions

--- script.js
+++ script.js
@@ -3,10 +3,42 @@ const prompt = require('prompt-sync')();
 // ------------------------------
 // Initialization
 // ------------------------------
+const backPages = new Stack();
 
+const NextPages = new Stack();
+
+let currentPage = 'Home Page';
 // ------------------------------
 // Helper Functions
 // ------------------------------
+function showCurrentPage(action){
+    console.log(`/n${action}`);
+    console.log(`Current page = ${currentPage}`);
+    console.log(`Back Page =`, backPages.peek());
+    console.log('Next page = ', nextPages.peek());
+};
+
+function newPage(page){
+  backPages.push(currentPage);
+  currentPage = page;
+  while(!nextPages.isEmpty()){
+    nextPages.pop();
+  }
+  showCurrentPage('NEW: ');
+};
+
+function backPage(){
+  nextPages.push(currentPage);
+  currentPage = backPages.pop()
+  showCurrentPage("BACK: ")
+};
+
+function nextPage(){
+  backPages.push(currentPage);
+  currentPage = nextPages.pop();
+  showCurrentPage("NEXT: ")
+
+}
 
 /*
  * The following strings are used to prompt the user
